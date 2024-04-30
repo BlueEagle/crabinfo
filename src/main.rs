020fbae07379f011a1ed1a973418d9f058e5b743
Collin Ballou <BlueEagle@users.noreply.github.com>
2024-04-29 23:10:14 -0600

    update implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use crabinfo;
 use std::process::{Command, Output};
-use std::str;
 
 fn profile_battery() -> Output {
     Command::new("system_profiler")
@@ -12,8 +11,6 @@ fn profile_battery() -> Output {
 fn main() {
     let system_info = crabinfo::SystemInformation::build();
     let battery_profile = profile_battery();
-    // let new_battery_profile = battery_profile.stdout.split("\n\n");
-    let new_battery_profile = str::from_utf8(&battery_profile.stdout).unwrap();
-    println!("{:?}\n", system_info);
-    println!("{:?}", new_battery_profile);
+    let new_battery_profile = String::from_utf8(battery_profile.stdout).unwrap();
+    println!("{:?}\n", new_battery_profile);
 }
