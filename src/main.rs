e063aac2d6767a6fcc897f94b1d6eed61e238418
Collin Ballou <BlueEagle@users.noreply.github.com>
2024-04-29 22:59:45 -0600

    testing pulling battery info

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 use crabinfo;
+use std::process::{Command, Output};
+use std::str;
+
+fn profile_battery() -> Output {
+    Command::new("system_profiler")
+        .arg("SPPowerDataType")
+        .output()
+        .expect("Failed to get system power information.")
+}
 
 fn main() {
     let system_info = crabinfo::SystemInformation::build();
-    println!("{:?}", system_info);
+    let battery_profile = profile_battery();
+    // let new_battery_profile = battery_profile.stdout.split("\n\n");
+    let new_battery_profile = str::from_utf8(&battery_profile.stdout).unwrap();
+    println!("{:?}\n", system_info);
+    println!("{:?}", new_battery_profile);
 }
